3318cafdb3382d94b09099d3222e3d46728bffde
Zohaib Javed <zohaib.javed@invozone.com>
2022-03-24 11:29:54 +0500

    first commit

---
+++ src/pages/components/community.js
@@ -0,0 +1,55 @@
+import React, { Component } from "react";
+import "../../assets/scss/community.scss";
+import image8 from "../../assets/images/image-8.png";
+import image9 from "../../assets/images/image-9.png";
+import image10 from "../../assets/images/image-10.png";
+import start from "../../assets/images/start.png";
+
+function community() {
+  return (
+    <div className="Community">
+      <div className="container">
+        <div className=" row">
+          <div className="col-lg-3">
+            <div className="communityContent">
+              <h2>Join the community campervan sites managers.</h2>
+              <p>
+                Discover all the equibbed areas in Italy. Find the shop with all
+                the services for you and your camper.
+              </p>
+
+              <button className="heroButton">
+                <img className="me-2" src={start} /> Start
+              </button>
+            </div>
+          </div>
+          <div className="col-lg-2 col-md-4 offset-lg-1">
+            <div className="communityImage">
+              <img src={image8} />
+              <div className="imagesHeading">Lucciano</div>
+              <p>Rome</p>
+              <p className="siteColor">View the Site</p>
+            </div>
+          </div>
+          <div className="col-lg-2 col-md-4 offset-lg-1">
+            <div className="communityImage">
+              <img src={image9} />
+              <div className="imagesHeading">Lucciano</div>
+              <p>Rome</p>
+              <p className="siteColor">View the Site</p>
+            </div>
+          </div>
+          <div className="col-lg-2 col-md-4  offset-lg-1">
+            <div className="communityImage">
+              <img src={image10} />
+              <div className="imagesHeading">Lucciano</div>
+              <p>Rome</p>
+              <p className="siteColor">View the Site</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default community;
