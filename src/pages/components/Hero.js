bff3319fff2dce606b5d7ce2037d6403c48afbab
Zohaib Javed <zohaib.javed@invozone.com>
2022-03-25 17:38:20 +0500

    ui fixed

---
+++ src/pages/components/Hero.js
@@ -0,0 +1,43 @@
+import React, { Component } from "react";
+import "../../assets/scss/hero.scss";
+import headerImage from "../../assets/images/cube-1.png";
+import headerImage2 from "../../assets/images/headerImage.png";
+import explore from "../../assets/images/explore.png";
+
+function Hero() {
+  return (
+    <div className="heroSection">
+      <div class="bg_image">
+        <div className="container">
+          <div className="row">
+            <div className="headerImage">
+              <img src={headerImage} />
+              <img className="headerImage2" src={headerImage2} />
+            </div>
+          </div>
+          <div className=" row">
+            <div className="col-lg-4">
+              <div className="bgColor">
+                <div className="heroContent">
+                  <h2>
+                    Find out how we can <span>save your time </span>in
+                    Backpacking in Europe
+                  </h2>
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ac
+                    amet id eget scelerisque amet etiam in sit commodo. Pretium
+                    ut duis orci pulvinar pretium id consectetur.{" "}
+                  </p>
+                  <button className="heroButton">
+                    <img className="me-2" src={explore} /> Explore
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default Hero;
---
+++ src/pages/components/cards.js
@@ -0,0 +1,71 @@
+import React, { Component } from "react";
+import "../../assets/scss/cards.scss";
+import image1 from "../../assets/images/image-1.png";
+import image2 from "../../assets/images/image-2.png";
+import image3 from "../../assets/images/image-3.png";
+
+function Hero() {
+  return (
+    <div className="Promoting">
+      <div className="container">
+        <div className="SoftwareCompnie">
+          <div className="row justify-content-center pt-5">
+            <div className="col-lg-4 col-md-6">
+              <div className="cards">
+                <div className="justify-content-center row">
+                  <div className="software-compnie-card">
+                    <img className="img-fluid" src={image1} alt="" />
+                    <h5>
+                      Promoting your territory <br /> towards foreign tourists
+                    </h5>
+                    <p>
+                      EasyCamper wants to promote the excellence of Italian food
+                      and wine, art, culture and the economy.
+                    </p>
+                    <h3>
+                      Discover the places linked to your area that you can link
+                      to your structure
+                    </h3>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-4 col-md-6">
+              <div className="justify-content-center row">
+                <div className="software-compnie-card">
+                  <img className="img-fluid" src={image2} alt="" />
+                  <h5>Promoting your territory towards foreign tourists</h5>
+                  <p>
+                    EasyCamper wants to promote the excellence of Italian food
+                    and wine, art, culture and the economy.
+                  </p>
+                  <h3>
+                    Discover the places linked to your area that you can link to
+                    your structure
+                  </h3>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-4 col-md-6">
+              <div className="justify-content-center row">
+                <div className="software-compnie-card">
+                  <img className="img-fluid" src={image3} alt="" />
+                  <h5>Promoting your territory towards foreign tourists</h5>
+                  <p>
+                    EasyCamper wants to promote the excellence of Italian food
+                    and wine, art, culture and the economy.
+                  </p>
+                  <h3>
+                    Discover the places linked to your area that you can link to
+                    your structure
+                  </h3>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default Hero;
